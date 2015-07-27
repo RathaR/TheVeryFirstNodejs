1d46cc51b628eb2481cfa1bc4d09388c410b0d7e
Ivan Pecharitsa <ivanp@compatibl.com>
2015-07-27 13:31:56 +0300

    remove idea files

--- spec.js
+++
@@ -1,7 +0,0 @@
-describe('Demo test ', function() {
-    it('should open web page', function () {
-        browser.get('http://www.protractortest.org/#/tutorial');
-        var body = element(by.css('body'));
-        expect(body.isDisplayed()).toBeTruthy();
-    });
-});
